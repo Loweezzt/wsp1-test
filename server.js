890ca6bf81db0dc26eb15afb39502d8fa54bdba2
Loweezzt <lowe.vesterberg@elev.ga.ntig.se>
2025-09-02 08:48:23 +0200

    Baseline för webbserver, lul

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send("Hello gais ")
 })
 
+app.get("/about", (req, res) => {
+    console.log(req)
+    res.send("<h1>Hello te23 välkomna!<h1>")
+    })
 app.listen(3000,() => {
     console.log("server is running on http://localhost:3000")
 })

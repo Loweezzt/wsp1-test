3fa12a7f837e5b6d3b95b7300171dac26a4fffea
Loweezzt <lowe.vesterberg@elev.ga.ntig.se>
2025-09-02 09:36:23 +0200

    lektion 2 eller 3 klar

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 import express from "express"
-
+import nunjucks from "nunjucks"
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send("Hello gais ")
+app.use(express.static("public"))
+
+nunjucks.configure("veiws", {
+    autoescape: true,
+    express: app
 })
 
 app.get("/about", (req, res) => {
     console.log(req)
     res.send("<h1>Hello te23 välkomna!<h1>")
     })
+
+
+app.get("/", (req, res) => {
+    res.render("index.njk", { title: "Vår första dynamiska sida", message: "Med Nunjucks skapar vi magi!" })
+})
+
+
+
 app.listen(3000,() => {
     console.log("server is running on http://localhost:3000")
 })
